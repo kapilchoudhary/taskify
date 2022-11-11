da5bfe9e15bf6383b55da02074280aad0411b78d
kapilchoudhary <kchoudhary006@gmail.com>
2022-11-11 16:39:50 +0530

    minor chnages for project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,9 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "trix"
 import "@rails/actiontext"
+
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   has_rich_text :description
+  belongs_to :user
+  has_many :tasks
+
+  enum :status, [:published, :archived ], default: :archived
 end
